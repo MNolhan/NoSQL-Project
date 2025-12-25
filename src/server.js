f71f044fc448d28173ed4324158e446eb2f46b50
Nolhan <nolhan.marteau@gmail.com>
2025-12-25 12:25:37 +0100

    Feature : Modification server routes

--- src/server.js
+++ src/server.js
@@ -1,10 +1,22 @@
 import express from 'express';
 import { configDotenv } from 'dotenv';
+import Create from './routes/sql/CreateUser.js';
+import Login from './routes/sql/Login.js';
+import Delete from './routes/sql/DeleteUser.js';
+import Read from './routes/sql/ReadUser.js';
+import Update from './routes/sql/UpdateUser.js';
 
 configDotenv();
 
 const PORT = process.env.PORT;
 const app = express();
+
+app.use(express.json());
+app.use("/CreateUser", Create);
+app.use("/Login", Login);
+app.use("/DeleteUser", Delete);
+app.use("/ReadUser", Read);
+app.use("/UpdateUser", Update);
 
 app.get('/test', (req, res) => {
     res.send('Hello, World!');
