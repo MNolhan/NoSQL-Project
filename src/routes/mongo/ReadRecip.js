53973a5adbb135b3ef054e487eed40958642fc30
Nolhan <nolhan.marteau@gmail.com>
2026-01-12 07:33:48 +0100

    Chore : Ajout du Cache pour la lecture des recettes

--- src/routes/mongo/ReadRecip.js
+++ src/routes/mongo/ReadRecip.js
@@ -1,20 +1,30 @@
 import express from "express";
-import { ObjectId } from "mongodb";
+import redisClient from "../redis/RedisClient.js";
 import { getRecipesCollection } from "../../db/mongo.js";
 
 const router = express.Router();
 
 router.get("/", async (req, res) => {
-    
     try {
+        const cacheKey = "recipes:all";
+        const cached = await redisClient.get(cacheKey);
+
+        if (cached) {
+            res.setHeader("X-Cache", "HIT");
+            return res.status(200).json(JSON.parse(cached));
+        }
 
         const col = await getRecipesCollection();
         const recipes = await col.find().sort({ createdAt: -1 }).toArray();
-        
-        res.json(recipes);
 
+        await redisClient.set(cacheKey, JSON.stringify(recipes), {
+        EX: 300,
+        });
+
+        res.setHeader("X-Cache", "MISS");
+        return res.status(200).json(recipes);
     } catch (err) {
-        res.status(500).json({ error: err.message });
+        return res.status(500).json({ error: err.message });
     }
 });
 
